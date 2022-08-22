45696a085f2378f622bcde0e1a1a5ffa42c49e5e
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2022-08-21 21:55:54 -0300

    refactor: move constant

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import data from '../data.json';
 
+const THREE = 3;
 const amountContainer = document.getElementsByClassName('expenses-chart__amount') as HTMLCollectionOf<HTMLElement>;
 const graphs = document.getElementsByClassName('expenses-chart__bar') as HTMLCollectionOf<HTMLElement>;
 const date = new Date().toUTCString().toLowerCase().slice(0, 3);
@@ -13,7 +14,6 @@ const handleGraphicAmounts = (): void => {
 
 const handleGraphics = (): void => {
   const graphList: HTMLElement[] = [...graphs];
-  const THREE = 3;
   graphList.forEach((graph: HTMLElement, index: number) => {
     graph.style.height = `${THREE * data[index].amount}px`;
     if (data[index].day === date) graph.style.backgroundColor = 'hsl(186, 34%, 60%)';
